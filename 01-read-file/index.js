19faf2d247bbdb32cf30792d6c1464a463424ec7
ek-ole <ek_34@mail.ru>
2024-01-23 00:03:38 +0300

    feat: add solution to 01-read-file with path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
+const path = require('path');
 
-/* fs.readFile('./text.txt', { encoding: 'utf-8' }, (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    console.log('File read successfully! Here is the data');
-    console.log(data);
- }
-}); */
-
-const readableStream = fs.createReadStream('text.txt', 'utf-8');
+const filePath = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(filePath, 'utf-8');
 readableStream.on('data', (chunk) => console.log(chunk));
